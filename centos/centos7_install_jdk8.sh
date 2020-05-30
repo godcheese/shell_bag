d17f31b7c8ebb60636a6438c71ca39b896102acb
godcheese <godcheese@outlook.com>
2020-05-30 20:35:40 +0800

    Initial commit

--- centos/centos7_install_jdk8.sh
+++ centos/centos7_install_jdk8.sh
@@ -9,7 +9,7 @@ temp_path=/tmp
 echo whoami
 function install_jdk8() {
 user=$(env | grep USER | cut -d "=" -f 2)
-echo user
+echo ${user}
     if [[ ${user} != "root" ]]; then
         echo -e "\033[31m 这个脚本必须用root执行！ \033[0m"
         exit
