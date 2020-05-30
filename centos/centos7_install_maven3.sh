38d34a3bcbcffb9f2b81c0448d742792dec48460
godcheese <godcheese@outlook.com>
2020-05-30 20:26:41 +0800

    Initial commit

--- centos/centos7_install_maven3.sh
+++ centos/centos7_install_maven3.sh
@@ -7,7 +7,7 @@ webwork_path=/webwork
 temp_path=/tmp
 
 function install_maven3() {
-    if [[ ${USER} != "root" ]]; then
+    if [[ whoami != "root" ]]; then
         echo -e "\033[31m 这个脚本必须用root执行！ \033[0m"
         exit
     fi
